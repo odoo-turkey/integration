0962adb75c26d7e2670fc66475acd0f6dcf126a8
yiğit budak <yibudak@gmail.com>
2022-04-22 12:25:33 +0300

    [12.0][ADD] verimor_bulutsantralim_click2dial

---
+++ verimor_bulutsantralim_click2dial/models/res_users.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Yiğit Budak (https://github.com/yibudak)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    internal_number = fields.Char(
+        string='Internal Number', copy=False,
+        help="User's internal phone number.")
+
+    _sql_constraints = [
+        ('unique_internal_number', 'unique (internal_number, id)', 'Internal number must be unique per user!'),
+    ]
